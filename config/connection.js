830779c299efccf8831de327b870f982a79a5c02
Shaquille Rivera <shaq.rivera@gmail.com>
2022-03-15 17:36:46 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,8 @@ const connectionString =
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = connection;
