37f99fe63074da31b654b0821b86f3f6f7a34474
Shaquille Rivera <shaq.rivera@gmail.com>
2022-03-15 17:51:55 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,7 @@ const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
 
 connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  
 });
 
 module.exports = connection;
